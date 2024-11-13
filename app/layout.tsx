eea3d59c134bac3ef565a3c7081271d34b46941c
tdeshsofresh <126199101+tdeshsofresh@users.noreply.github.com>
2024-11-12 20:12:47 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Historical Figures Bingo",
@@ -33,9 +21,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/5_Point_Star_with_Army_Star_Border.svg/32px-5_Point_Star_with_Army_Star_Border.svg.png" sizes="any" />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased font-sans">
         {children}
       </body>
     </html>
